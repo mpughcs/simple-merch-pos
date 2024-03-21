959c835279a3c9d737baa553ccb54e91f7185876
Max Pugh <mpugh@chapman.edu>
2024-03-20 19:17:03 -0700

    fixed home page styling

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -49,25 +49,25 @@ function Home() {
     };
 
     return (
-        <div className="flex flex-col text-center py-20 ">
-            <h1 className='text-2xl'>Sunday Mourners Merch Manager</h1>
-            <img src={logo} className='w-[40%] mx-auto my-5' alt="SM logo" />
-            <div className='flex flex-col justify-start gap-10'>
-                <div className='flex-col mx-auto '>
-                    <button className='hover:text-pink-400' onClick={() => setShowAddSession(!showAddSession)}>Create Session</button>
+        <div className="flex flex-col  py-20 w-[60%] max-w-[450px] mx-auto my-5 ">
+            <h1 className='text-2xl sm:text-4xl text-center'>Sunday Mourners Merch Manager</h1>
+            <img src={logo} className=' my-5' alt="SM logo" />
+            <div className='flex flex-col  gap-10'>
+                <div className=''>
+                    <button className='hover:text-pink-400 text-2xl' onClick={() => setShowAddSession(!showAddSession)}>Create Session</button>
                     {showAddSession && <div className='flex  gap-5 py-2'>
-                        <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal text-black w-[300px] " placeholder="Venue Name" onChange={(e) => setVenueName(e.target.value)} />
+                        <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal text-black w-[300px] " placeholder="Session Name" onChange={(e) => setVenueName(e.target.value)} />
                         <FaArrowRight className='text-2xl my-auto hover:text-pink-400 hover:cursor-pointer' onClick={handleCreateSession} />
                     </div>}
                 </div>
-                <div className='flex-col mx-auto '>
-                    <button className='hover:text-pink-400' onClick={() => setShowResumeSession(!showResumeSession)}>Resume Session</button>
+                <div className='f '>
+                    <button className='hover:text-pink-400 text-2xl' onClick={() => setShowResumeSession(!showResumeSession)}>Resume Session</button>
                     {showResumeSession && <div className='flex  gap-5 py-2'>
                         <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal text-black w-[300px] " placeholder="session id" onChange={(e) => setSessionId(e.target.value)} />
                         <FaArrowRight className='text-2xl my-auto hover:text-pink-400 hover:cursor-pointer' onClick={() => handleResumeSession(sessionId)} />
                     </div>}
                 </div>
-                <button className='hover:text-pink-400' onClick={handleLogout}>Log Out</button>
+                <button className='hover:text-pink-400 text-2xl w-fit' onClick={handleLogout}>Log Out</button>
             </div>
         </div>
     );
